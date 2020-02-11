62ec3f87d6057872f138632c51e17da555ec013d
Pratima Gokhale <pratima.gokhale@gmail.com>
2020-02-11 15:00:12 -0600

    Flask app completed

--- MyClimateApp.py
+++ MyClimateApp.py
@@ -6,7 +6,7 @@ from sqlalchemy import and_, func
 from sqlalchemy.orm import Session
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Resources/hawaii.sqlite'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Resources/hawaii.sqlite"
 db = SQLAlchemy(app)
 
 
@@ -19,15 +19,17 @@ class DictMixIn:
             for column in self.__table__.columns
         }
 
-class Measurement(db.Model,DictMixIn):
+
+class Measurement(db.Model, DictMixIn):
     __tablename__ = "measurement"
-    id =  db.Column(db.Integer(), primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)
     station = db.Column(db.String())
     date = db.Column(db.String())
     prcp = db.Column(db.Float())
     tobs = db.Column(db.Float())
-    
-class Station(db.Model,DictMixIn):
+
+
+class Station(db.Model, DictMixIn):
     __tablename__ = "station"
     id = db.Column(db.Integer(), primary_key=True)
     station = db.Column(db.String())
@@ -35,6 +37,7 @@ class Station(db.Model,DictMixIn):
     latitude = db.Column(db.Float())
     longitude = db.Column(db.Float())
     elevation = db.Column(db.Float())
+
 
 def calc_temps(start_date, end_date):
     """TMIN, TAVG, and TMAX for a list of dates.
@@ -46,66 +49,67 @@ def calc_temps(start_date, end_date):
     Returns:
         TMIN, TAVE, and TMAX
     """
-   
-    Temperatures = db.session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-        filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
-   
+
+    Temperatures = (
+        db.session.query(
+            func.min(Measurement.tobs),
+            func.avg(Measurement.tobs),
+            func.max(Measurement.tobs),
+        )
+        .filter(Measurement.date >= start_date)
+        .filter(Measurement.date <= end_date)
+        .all()
+    )
 
     return Temperatures
 
 
-
 @app.route("/")
 def home():
-    return render_template("HomePage.html") 
+    return render_template("HomePage.html")
 
 
 @app.route("/api/v1.0/precipitation")
 def show_all():
-    cols = ['date', 'prcp']
-    Precipitations = Measurement.query\
-        .filter(
+    cols = ["date", "prcp"]
+    Precipitations = Measurement.query.filter(
         and_(
-            Measurement.date > datetime.datetime(2016,8,23),
-            Measurement.date <= datetime.datetime(2017,8,23),           
-        )\
+            Measurement.date > datetime.datetime(2016, 8, 23),
+            Measurement.date <= datetime.datetime(2017, 8, 23),
         )
+    )
     result1 = [{col: getattr(Precip, col) for col in cols} for Precip in Precipitations]
-    result = [{r["date"] : r["prcp"]} for r in result1]
+    result = [{r["date"]: r["prcp"]} for r in result1]
     return jsonify(result=result)
-        
+
 
 @app.route("/api/v1.0/stations")
 def show_stations():
     station_names = Station.query.all()
-    cols = ['name']
+    cols = ["name"]
     result = [{col: getattr(name, col) for col in cols} for name in station_names]
     return jsonify(result=result)
 
+
 @app.route("/api/v1.0/tobs")
 def show_temperature():
-    cols = ['date', 'tobs']
-    Temperatures = Measurement.query\
-        .filter(
+    cols = ["date", "tobs"]
+    Temperatures = Measurement.query.filter(
         and_(
-            Measurement.date > datetime.datetime(2016,8,23),
-            Measurement.date <= datetime.datetime(2017,8,23),           
-        )\
+            Measurement.date > datetime.datetime(2016, 8, 23),
+            Measurement.date <= datetime.datetime(2017, 8, 23),
         )
+    )
     result1 = [{col: getattr(Temp, col) for col in cols} for Temp in Temperatures]
-    result = [{r["date"] : r["tobs"]} for r in result1]
+    result = [{r["date"]: r["tobs"]} for r in result1]
     return jsonify(result=result)
 
-# `/api/v1.0/<start>` and `/api/v1.0/<start>/<end>`
 
-# @app.route("/api/v1.0/<start>")
-# def show_temp_stat(start):
-
-#     for pet in pets:
-#         if pet["id"] == int(pet_id):
-#             return f'Name: {pet["name"]}, Age: {pet["age"]}, Type: {pet["type"]}, Color: {pet["color"]}'
-
-#     return "Pet not found!", 404
+def validate(date_text):
+    try:
+        datetime.datetime.strptime(date_text, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Incorrect date format, should be YYYY-MM-DD")
 
 
 @app.route("/api/v1.0")
@@ -113,33 +117,40 @@ def filter_temp_by_date():
 
     request_start = request.args.get("start")
     request_end = request.args.get("end")
+
     try:
-           
+
         if request_start:
-            #start_date = datetime.datetime.strptime(request_start, "%Y-%m-%d")
+            validate(request_start)
             start_date = request_start
-            print(start_date)        
-        
+        else:
+            return "Please enter the start date"
+
         if request_end:
-            end_date = datetime.datetime.strptime(request_end, "%Y-%m-%d")
+            validate(request_end)
             end_date = request_end
-            print(end_date)
+
         else:
             max_date = db.session.query(func.max(Measurement.date)).all()
             end_date = [date[0] for date in max_date]
             end_date = end_date[0]
-            
+        if end_date > start_date:
+            data = calc_temps(start_date, end_date)
+        else:
+            return "End date should be greater than start date"
 
-        data = calc_temps(start_date, end_date)
-        
         list_stat = [[temp[0], temp[1], temp[2]] for temp in data][0]
         tmin = list_stat[0]
         tavg = list_stat[1]
         tmax = list_stat[2]
-        return jsonify({"Tmin": tmin, "Tavg": tavg, "Tmax": tmax})
+        if tmin is None or tavg is None or tmax is None:
+            return "Temperatures not found!", 404
+        else:
+            return jsonify({"Tmin": tmin, "Tavg": tavg, "Tmax": tmax})
 
     except Exception as e:
         return jsonify({"status": "failure", "error": str(e)})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
